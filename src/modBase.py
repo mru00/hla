ec4293c7ba9fe60f1a998687a0bd80497773a0d5
mru <mru@sisyphus.teil.cc>
2011-03-27 17:26:36 +0200

    refactored parser -> removed all syntax definition

---
+++ src/modBase.py
@@ -0,0 +1,89 @@
+from ruledb import *
+from entities import *
+from namespace import *
+from mako.template import Template
+
+
+class Name(TE): pass
+
+class VarRef(TE):
+    def canparse(self):
+        return get_symbol('var', self.value) != None
+
+class Value(TE):
+    expression = re.compile(r"\d+|low|high")
+
+class Op(TE):
+    expression = re.compile(r"[+-=]")
+
+class Type(TE):
+    expression = re.compile(r"u8")
+
+
+
+class Target(NTE):
+    genpos = GEN_INCLUDE
+    
+
+
+class Program(NTE):
+
+    template = Template(r"""
+#include <avr.h>
+
+% for o in nte.getpositems(gendb.GEN_INCLUDE):
+<% assert o != Undefined %>
+${o.render(0)}\
+% endfor
+
+% for o in nte.getpositems(gendb.GEN_STATIC_GLOBAL):
+<% assert o != Undefined %>
+${o.render(0)}\
+% endfor
+
+void main() {
+
+% for o in nte.getpositems(gendb.GEN_INITIALLY_MAIN):
+<% assert o != Undefined %>
+${o.render(1)}
+% endfor
+
+  for(;;) {
+% for o in nte.getpositems(gendb.GEN_MAIN_LOOP):
+<% assert o != Undefined %>
+${o.render(3)}
+% endfor
+  }
+
+}
+
+""")
+
+    def getpositems(self, pos):
+        return filter (lambda a: a.genpos == pos, self.items)
+
+
+class Decl(NTE): pass
+class Statement(NTE): pass
+class Condition(NTE): pass
+
+
+class UseDecl(Decl):
+    def onparse(self):
+        load_mod(self.items[1].value)
+
+class VarDecl(Decl):
+    template = Template("""
+static ${nte.items[3].value} ${nte.items[1].value};
+""")
+
+    genpos = GEN_STATIC_GLOBAL
+    def onparse(self):
+        add_symbol('var', self.items[1].value, self)
+
+add_rule(Target, [ "targets", Name ])
+add_rule(UseDecl, [ "uses", Name ])
+add_rule(VarDecl, [ "var", Name, "is", Type ])
+
+add_rule(Program, [ Target , [Decl]])
+
